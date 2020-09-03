02f2b99b725608662d40ffef20550daf0eb4d588
Alexis Haldy <alexishaldy@gmail.com>
2020-09-03 09:06:22 +0200

    Add mthod for MovableItem

--- src/main/java/ch/cpnv/roguetale/entity/MovableItem.java
+++ src/main/java/ch/cpnv/roguetale/entity/MovableItem.java
@@ -1,6 +1,7 @@
-package ch.cpnv.roguetale.entity;
+package main.java.ch.cpnv.roguetale.entity;
 
 import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SpriteSheet;
 
 public class MovableItem extends DrawableItem {
@@ -14,7 +15,25 @@ public class MovableItem extends DrawableItem {
 	}
 	
 	public void move(int delta) {
-		//this.setPosition(new Vector2f);
+		Vector2f pos = this.getPosition();
+		int movement = this.speed * delta;
+		
+		switch (this.direction) {
+			case UP:
+				pos.translate(0, movement);
+				break;
+			case DOWN:
+				pos.translate(0, -movement);
+				break;
+			case LEFT:
+				pos.translate(-movement, 0);
+				break;
+			case RIGHT:
+				pos.translate(movement, 0);
+				break;
+			default:
+				break;
+		}
 	}
 	
 	public void setDirection(Direction direction) {
@@ -23,6 +42,10 @@ public class MovableItem extends DrawableItem {
 	
 	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+	
+	public Image getSprite(int x, int y) {
+		return this.spritesheet.getSprite(x, y);
 	}
 
 }
