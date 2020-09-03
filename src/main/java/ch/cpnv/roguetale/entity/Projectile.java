66ada544b91618d16be144e9d2c610f995a73dd0
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-03 12:23:12 +0200

    Add Arrow

--- src/main/java/ch/cpnv/roguetale/entity/Projectile.java
+++ src/main/java/ch/cpnv/roguetale/entity/Projectile.java
@@ -1,12 +1,31 @@
 package main.java.ch.cpnv.roguetale.entity;
 
 import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SpriteSheet;
 
 public abstract class Projectile extends MovableItem {
-	protected int lifespan;
+	protected int remaining_time;
 	
-	public Projectile(SpriteSheet ss, Vector2f position, int speed, Direction direction) {
+	public Projectile(SpriteSheet ss, Vector2f position, int speed, Direction direction, int lifespan) {
 		super(ss, position, speed, direction);
+		this.remaining_time = lifespan;
+	}
+	
+	public Image getSprite() {
+		Image image = this.spritesheet.getSprite(0, 0);
+		switch (this.direction) {
+		case UP:
+			break;
+		case DOWN:
+			break;
+		case LEFT:
+			break;
+		case RIGHT:
+			break;
+		default:
+			break;
+	}
+		return image;
 	}
 }
