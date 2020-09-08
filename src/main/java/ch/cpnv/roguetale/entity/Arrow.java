395193a7e9523a2141b15a230ecfce075c8953a3
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-08 09:49:56 +0200

    Update arrow image

--- src/main/java/ch/cpnv/roguetale/entity/Arrow.java
+++ src/main/java/ch/cpnv/roguetale/entity/Arrow.java
@@ -5,9 +5,9 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
 public class Arrow extends Projectile {
-	static protected final int SPEED = 10;
+	static protected final int SPEED = 1;
 	static protected final int LIFESPAN = 500;
-	static protected final String SPRITESHEET_PATH = "main\\java\\ch\\cpnv\\roguetale\\images\\projectiles\\IS - Arrow 568.png";
+	static protected final String SPRITESHEET_PATH = "main\\java\\ch\\cpnv\\roguetale\\images\\projectiles\\arrow.png";
 
 	public Arrow(Vector2f position, Direction direction) throws SlickException {
 		super(
