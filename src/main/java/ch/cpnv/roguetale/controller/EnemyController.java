5bdb2730193f1d6f018d078a2de662cc7259a38e
Alexis Haldy <alexishaldy@gmail.com>
2020-09-10 11:30:24 +0200

    Create enemyController

---
+++ src/main/java/ch/cpnv/roguetale/controller/EnemyController.java
@@ -0,0 +1,40 @@
+package ch.cpnv.roguetale.controller;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+import ch.cpnv.roguetale.entity.character.Player;
+
+public class EnemyController implements Controller {
+	
+
+	public EnemyController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g, Player p) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta, Player p) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(int key, char c, GameContainer gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(int key, char c, GameContainer gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
