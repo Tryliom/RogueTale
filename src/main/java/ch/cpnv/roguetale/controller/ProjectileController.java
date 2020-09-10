773b94fdfbc9a34a6abf7fae03e688d069369407
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-10 10:36:14 +0200

    Add projectileController

---
+++ src/main/java/ch/cpnv/roguetale/controller/ProjectileController.java
@@ -0,0 +1,35 @@
+package ch.cpnv.roguetale.controller;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+import ch.cpnv.roguetale.entity.character.Player;
+
+public class ProjectileController implements Controller {
+
+	@Override
+	public void render(GameContainer gc, Graphics g, Player p) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta, Player p) throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(int key, char c, GameContainer gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(int key, char c, GameContainer gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
