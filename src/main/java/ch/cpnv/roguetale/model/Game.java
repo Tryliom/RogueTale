9df577adc466e4d9db575b7c91b3359a3931ffd6
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-15 12:21:40 +0200

    Add life display test

--- src/main/java/ch/cpnv/roguetale/model/Game.java
+++ src/main/java/ch/cpnv/roguetale/model/Game.java
@@ -24,6 +24,8 @@ public class Game extends BasicGame {
 	private int height;
 	@SuppressWarnings("unused")
 	private int score;
+	
+	private int testTimer = 0;
 
 	public Game() {
 		// Title windows name
@@ -64,6 +66,13 @@ public class Game extends BasicGame {
 		PlayerController.getInstance().update(gc, delta);
 		ProjectileController.getInstance().update(gc, delta);
 		this.uiController.update(gc, delta, player);
+		
+		// Test player health
+		testTimer += delta;
+		if (testTimer >= 1000) {
+			player.updateHealth(-1);
+			testTimer -= 1000;
+		}
 	}
 	
 	@Override
