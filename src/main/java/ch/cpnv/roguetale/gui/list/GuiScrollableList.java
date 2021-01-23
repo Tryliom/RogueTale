dfe8de5a435ee9fcebbd013d601883928b308078
Tryliom <alexishaldy@gmail.com>
2021-01-23 13:28:25 +0100

    Update arrow image

--- src/main/java/ch/cpnv/roguetale/gui/list/GuiScrollableList.java
+++ src/main/java/ch/cpnv/roguetale/gui/list/GuiScrollableList.java
@@ -3,7 +3,6 @@ package ch.cpnv.roguetale.gui.list;
 import java.util.ArrayList;
 
 import org.lwjgl.util.vector.Vector2f;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
